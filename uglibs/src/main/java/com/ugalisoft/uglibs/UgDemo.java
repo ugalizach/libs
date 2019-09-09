7e31a234223303cc0d469a68e6004e7677f77172
ugali <zmwalwama@gmail.com>
2019-09-10 00:04:17 +0300

    first commit

---
+++ uglibs/src/main/java/com/ugalisoft/uglibs/UgDemo.java
@@ -0,0 +1,11 @@
+package com.ugalisoft.uglibs;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class UgDemo {
+
+    public UgDemo(Context context){
+        Toast.makeText(context,"Here",Toast.LENGTH_LONG).show();
+    }
+}
